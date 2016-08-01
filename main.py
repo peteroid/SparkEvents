9d94afad4054529211c4bba357a24d2ab0baf6c2
PETEroid <peteroid.howinai@gmail.com>
2016-08-01 12:02:04 -0700

    Fix typo in main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def parse_dir(path):
 
 if __name__ == "__main__":
     if len(sys.argv) < 1:
-        PATH = os.path.join("smaple_data")
+        PATH = os.path.join("sample_data")
     else:
         PATH = os.path.join(sys.argv[1])
     parse_dir(PATH)
